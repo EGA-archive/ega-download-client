0f71fe98b51dad284df85df21f7894ff71c13006
kipper <cafletezbrant@gmail.com>
2018-10-23 15:34:52 -0700

    fix unit_divisor error message

--- pyega3/pyega3.py
+++ pyega3/pyega3.py
@@ -271,7 +271,7 @@ def download_file( token, file_id, file_size, check_sum, num_connections, key, o
 
     chunk_len = math.ceil(file_size/num_connections)
 
-    with tqdm(total=int(file_size), unit='B', unit_scale=True, unit_divisor=1024) as pbar:
+    with tqdm(total=int(file_size), unit='B', unit_scale=True) as pbar:
         params = [(url, token, output_file, chunk_start_pos, min(chunk_len,file_size-chunk_start_pos), pbar) for chunk_start_pos in range(0,file_size, chunk_len)]        
 
         results = []
