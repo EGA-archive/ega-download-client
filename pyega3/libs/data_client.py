ec43e5d09fceee303be20c7753b0b703d33cc710
Alegria Aclan <aaclan@ebi.ac.uk>
2022-05-04 18:55:49 +0100

    Use retry policy for retrying get stream requests

--- pyega3/libs/data_client.py
+++ pyega3/libs/data_client.py
@@ -3,6 +3,29 @@ import json
 import logging
 
 import requests
+from requests import Session
+from requests.adapters import HTTPAdapter
+from urllib3.util import retry
+
+
+def create_session_with_retry(retry_policy=None) -> Session:
+    retry_policy = retry_policy or retry.Retry(
+        total=50,
+        connect=False,
+        # seems that this has a default value of 10,
+        # setting this to a very high number so that it'll respect the status retry count
+
+        status=10,
+        # status is the no. of retries if response is in status_forcelist
+
+        read=10,
+        backoff_factor=0.6
+    )
+    session = Session()
+    adapter = HTTPAdapter(max_retries=retry_policy)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    return session
 
 
 class DataClient:
@@ -12,6 +35,7 @@ class DataClient:
         self.htsget_url = htsget_url
         self.auth_client = auth_client
         self.standard_headers = standard_headers
+        self.session = create_session_with_retry()
 
     @staticmethod
     def print_debug_info(url, reply_json, *args):
@@ -43,7 +67,7 @@ class DataClient:
             headers.update(extra_headers)
 
         url = f'{self.url}{path}'
-        with requests.get(url, headers=headers, stream=True) as r:
+        with self.session.get(url, headers=headers, stream=True) as r:
             self.print_debug_info(url, None, f"Response headers: {r.headers}")
             r.raise_for_status()
             yield r
