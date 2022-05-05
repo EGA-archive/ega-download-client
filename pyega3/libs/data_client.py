3083aa29f0d1681900cfef504c8055f0f08f175d
Alegria Aclan <aaclan@ebi.ac.uk>
2022-05-05 08:57:54 +0100

    Improve order of retry params for readability

--- pyega3/libs/data_client.py
+++ pyega3/libs/data_client.py
@@ -8,24 +8,25 @@ from requests.adapters import HTTPAdapter, DEFAULT_POOLSIZE
 from urllib3.util import retry
 
 
-def create_session_with_retry(retry_policy=None, pool_max_size=None) -> Session:
+def create_session_with_retry(retry_policy: retry.Retry = None, pool_max_size=None) -> Session:
     retry_policy = retry_policy or retry.Retry(
-        total=50,
-        connect=False,
-        # seems that this has a default value of 10,
-        # setting this to a very high number so that it'll respect the status retry count
-
-        status=10,
-        # status is the no. of retries if response is in status_forcelist
-
         status_forcelist=[429, 500, 503, 504],
+        # status is the no. of retries if response is in status_forcelist
+        status=10,
+        # total has a default value of 10,
+        # need to set total to a number higher than status so it'll respect the status retry count
+        total=20,
+        # do not retry connection errors
+        connect=False,
         read=10,
+        # 0.3, 0.6, 1.2, 2.4, 4.8, 9.6, 19.2, 38.4, 76.8, 120 is the BACKOFF_MAX in Retry
         backoff_factor=0.6
     )
     session = Session()
     POOL_MAX_SIZE = max(DEFAULT_POOLSIZE, pool_max_size or 0)
     adapter = HTTPAdapter(max_retries=retry_policy, pool_maxsize=POOL_MAX_SIZE)
-    session.mount('http://', adapter)
+    # TODO check if this can be removed
+    # session.mount('http://', adapter)
     session.mount('https://', adapter)
     return session
 
