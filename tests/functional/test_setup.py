8da83eeaa5ee75f338cabd1dab8f1d0456cd5b97
Alegria Aclan <aaclan@ebi.ac.uk>
2022-05-31 14:18:19 +0100

    Assume pyega3 is already installed when running functional tests

--- tests/functional/test_setup.py
+++
@@ -1,9 +0,0 @@
-import re
-
-from tests.functional.conftest import PYEGA3_NAME, PYEGA3_VERSION
-
-
-def test_install(install_package):
-    exit_code, output, error = install_package
-    assert exit_code == 0
-    assert bool(re.search(f'Successfully installed {PYEGA3_NAME}-{PYEGA3_VERSION}', output))
