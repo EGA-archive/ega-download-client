55f3ff6d9af4e104d835443f62cd93bdfdc6eb05
Anna <31920806+afoix@users.noreply.github.com>
2022-07-11 18:01:34 +0100

    Update pyega3/pyega3.py
    Co-authored-by: andresfsilva <afsilva04@gmail.com>

--- pyega3/pyega3.py
+++ pyega3/pyega3.py
@@ -143,7 +143,7 @@ def main():
     auth_client.credentials = credentials
 
     data_client = DataClient(server_config.url_api, server_config.url_api_ticket, auth_client, standard_headers,
-                             connections=args.connections, metadata_url=server_config. url_api_metadata)
+                             connections=args.connections, metadata_url=server_config.url_api_metadata)
 
     execute_subcommand(args, data_client)
 
