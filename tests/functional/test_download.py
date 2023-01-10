1d2cd9c6a3686ac44c35f39eba3c6a48d93b389d
Alegria Aclan <aaclan@ebi.ac.uk>
2023-01-10 19:52:20 +0000

    Add functional tests for htsget

--- tests/functional/test_download.py
+++ tests/functional/test_download.py
@@ -8,31 +8,31 @@ script_dir = os.path.dirname(__file__)
 
 
 def test_download_file():
-    file = 'EGAF00001753741'
-    download_dir = f'{script_dir}/{file}'
+    file_id = 'EGAF00001753741'
+    download_dir = f'{script_dir}/{file_id}'
 
-    _assert_successful_run(f'pyega3 -t fetch {file} --output-dir {script_dir}')
+    run_command_and_assert_download_complete(f'pyega3 -t fetch {file_id} --output-dir {script_dir}')
     _assert_complete_files(download_dir)
-    _cleanup(download_dir)
+    cleanup(download_dir)
 
 
 def test_multipart_downloading():
-    file = 'EGAF00005001625'  # less 200MB, will create 2 slices
+    file_id = 'EGAF00005001625'  # less 200MB, will create 2 slices
     conns = 2  # only 2 will be utilised
-    download_dir = f'{script_dir}/{file}'
+    download_dir = f'{script_dir}/{file_id}'
 
-    _assert_successful_run(f'pyega3 -t -c {conns} fetch {file} --output-dir {script_dir}')
+    run_command_and_assert_download_complete(f'pyega3 -t -c {conns} fetch {file_id} --output-dir {script_dir}')
     _assert_complete_files(download_dir)
-    _cleanup(download_dir)
+    cleanup(download_dir)
 
 
 def test_download_dataset():
     dataset = 'EGAD00001009826'
     download_dir = f'{script_dir}/{dataset}'
     os.makedirs(download_dir, exist_ok=True)
-    _assert_successful_run(f'pyega3 -t fetch {dataset} --output-dir {dataset}')
+    run_command_and_assert_download_complete(f'pyega3 -t fetch {dataset} --output-dir {dataset}')
     _assert_all_files_downloaded(download_dir)
-    _cleanup(download_dir)
+    cleanup(download_dir)
 
 
 def _assert_all_files_downloaded(download_dir):
@@ -41,7 +41,7 @@ def _assert_all_files_downloaded(download_dir):
         _assert_complete_files(f'{download_dir}/{d}')
 
 
-def _assert_successful_run(command: str):
+def run_command_and_assert_download_complete(command: str):
     exit_code, output, error = run(command)
     assert exit_code == 0
     output += error  # it seems that output is in stderr
@@ -54,6 +54,6 @@ def _assert_complete_files(file_dir):
     assert len(downloaded_files) == 2
 
 
-def _cleanup(download_dir):
+def cleanup(download_dir):
     shutil.rmtree(download_dir)
     os.remove('pyega3_output.log')
